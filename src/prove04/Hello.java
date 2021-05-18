de13493f4b5b05bb1634a0d854263c773f5d39df
Use your GitHub username <marindrake@gmail.com>
2021-05-17 19:17:47 -0600

    Update

--- src/prove04/Hello.java
+++ src/prove04/Hello.java
@@ -3,9 +3,6 @@ package prove04;
 public class Hello {
     public static void main(String []args){
         System.out.println("Hello, my name is Paola");
-        System.out.println("Hello, my name Alex");
-        
+        System.out.println("Hello Paola, my name is Alex.");
     }
-
-
 }
