5f1c0428883f60b56604a4aefed1b03b832ce0db
marindrake <83062629+marindrake@users.noreply.github.com>
2021-05-17 19:11:37 -0600

    Update Hello.java

--- src/prove04/Hello.java
+++ src/prove04/Hello.java
@@ -3,6 +3,8 @@ package prove04;
 public class Hello {
     public static void main(String []args){
         System.out.println("Hello, my name is Paola");
+        System.out.println("Hello, my name Alex");
+        
     }
 
 
